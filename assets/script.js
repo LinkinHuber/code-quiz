0535bb4271c6c4e89d363d9f9038a87a38faac58
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-04-03 23:23:24 -0500

    added a bunch of notes to JS file to know what I have to acomplish

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+//1. need an event listener to start the quiz via the press of a "button"
+
+//2. need a way to store all of the questions/answers... example
+  /*
+    {
+      name: "what is the best seafood?"
+      answers: [
+
+      ]
+    }
+  */
+//3. need a timer that counts down over the duration of the quiz
+
+//4. need a way to move onto the next question after a question is answered 
+
+//5. need a way to subtract time from the countdown if the question is answered wrong
+
+//6. need a way to stop the game after all the questions have been answered or the time ran out
+
+//7. need a way to then save the persons initials and their score
+
+
+
+// general layout to get the quiz started
+
+//user arrives at web page
+//click the start button
+  //first question appears
+  //timer starts
+
+  //listen for user to click on one of the answers
+
+//when an answer is clicked
+  //determine if the correct answer was clicked
+
+    //if it was correct => move onto next question without subtracting time
+
+    //if not correct => subtract time and move onto the next question
+    
