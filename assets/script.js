c84ad733c39a96fc36f8d2d0929204269fc39369
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-04-20 01:13:59 -0500

    got question to display on the screen

--- assets/script.js
+++ assets/script.js
@@ -1,108 +1,91 @@
-    var startButton = document.querySelector('.start-button');
-    var timer = document.querySelector('.timer-count');
-    var secondsLeft = 10;
-    var score = 0;
-
-    var qp1Button = document.querySelector('#qp-1');
-    var qp2Button = document.querySelector('#qp-2');
-    var qp3Button = document.querySelector('#qp-3');
-    
-
-    function hideStartPage() {
-      document.getElementById('intro-page').hidden = true;
-    }
-
-    function showQuestionPage1() {
-      document.getElementById('qp-1').hidden = false;
-    }
-    
-    function hideQuestionPage1() {
-      document.getElementById('qp-1').hidden = true;
-    }
-
-    function showQuestionPage2() {
-      document.getElementById('qp-2').hidden = false;
-    }
-   
-    function hideQuestionPage2() {
-      document.getElementById('qp-2').hidden = true;
-    }
-
-    function showQuestionPage3() {
-      document.getElementById('qp-3').hidden = false;
-    }
-
-    function hideQuestionPage3() {
-      document.getElementById('qp-3').hidden = true;
-    }
-    
-    function showScorePage() {
-      document.getElementById('scoreboard').hidden = false;
-    }
+  var btnStartEl = document.querySelector('.start-button');
+  var questionEl = document.getElementById('question');
+  var choiceButtons = document.getElementById('question-answers');
+  var timer = document.querySelector('.timer-count');
+  var secondsLeft = 10;
+  var score = 0;
+  var timeleft;
+  var gameover;
+  var arrOfQuestions = [];
+  var QuestionIndex = 0;
 
 
-      startButton.addEventListener('click', function(event){
-        event.preventDefault();
-          hideStartPage();
-          startTimer();
-          showQuestionPage1()
-          
-      });
+  var questions = [
+    { questionTitle: 'What is my first name?', 
+      rightAnswr: '1. Linkin', 
+      choices: [{choice: '1. Linkin '}, {choice: '2. Chad'}, {choice: '3. Dylan'}, {choice: '4. Dennis'}]
+    },
+    { q: 'How old am I?', 
+      a: '2. 20', 
+      choices: [{choice: '1. 19'}, {choice: '2. 20'}, {choice: '3. 18'}, {choice: '4. 21'}]
+    },
+  ];
 
-      qp1Button.addEventListener('click', function(event){
-        event.preventDefault();
-        hideQuestionPage1();
-        showQuestionPage2()
-      });
-        
-      qp2Button.addEventListener('click', function(event){
-        event.preventDefault();
-        hideQuestionPage2();
-        showQuestionPage3() 
-      });
 
-      qp3Button.addEventListener('click', function(event){
-        event.preventDefault();
-        hideQuestionPage3();
-        showScorePage() 
-      });
-      
-
-  function startTimer() {
-    var countDown = setInterval(function(){
-      secondsLeft--;
-      timer.textContent = secondsLeft;
-      if(secondsLeft === 0){
-        clearInterval(countDown);
-        hideQuestionPage1();
-        hideQuestionPage2();
-        hideQuestionPage3();
-        showScorePage()
-
-      }
-    },1000)
+//hides intro page and shows the question page. The first question is selected in a random order. 
+var startQuiz = function() {
+    document.getElementById('intro-page').hidden = true;
+    document.getElementById('question-container').hidden = false;
+    arrOfQuestions = questions.sort(() => Math.random() - 0.5)
+    startTimer()
+    setQuestion()
   }
 
+//set next question for quiz
+var setQuestion = function() {
+    resetAnswers()
+    displayQuestion(arrOfQuestions[QuestionIndex])
+}
 
-  //1. need a way to figure out what answers are right or wrong
+//remove answer buttons
+var resetAnswers = function() {
+    while (choiceButtons.firstChild) {
+        choiceButtons.removeChild(choiceButtons.firstChild)
+    };
+};
 
-  //2. need a way to keep track of the score and add points for right answers
+//display question information (including answer buttons)
+var displayQuestion = function(index) {
+    questionEl.innerText = index.q
+    for (var i = 0; i < index.choices.length; i++) {
+        var answerbutton = document.createElement('button')
+        answerbutton.innerText = index.choices[i].choice
+        answerbutton.classList.add('btn')
+        answerbutton.classList.add('answerbtn')
+        answerbutton.addEventListener('click', checkAnswr)
+        choiceButtons.appendChild(answerbutton)
+        }
+    };
 
-  //3. need a way to subtract time from the countdown if the question is answered wrong
+//check if answer is correct    
+var checkAnswr = function(event) {
+    var chosenAnswr = event.target
+        if (arrOfQuestions[QuestionIndex].a === chosenAnswr.innerText){
+            score = score + 1
+        }
 
-  //4. need a way to then save the persons initials and their score
-  
+        else {
+          timeleft = timeleft - 2;
+      };
 
-  function rightWrongAnswers() {
-   
-  }
+    //go to next question, check if there is more questions
+      QuestionIndex++
+        if  (arrOfQuestions.length > QuestionIndex + 1) {
+            setQuestion()
+        }   
+        else {
+           gameover = 'true';
+            }
+}
 
+function startTimer() {
+  var countDown = setInterval(function(){
+    secondsLeft--;
+    timer.textContent = secondsLeft;
+    if(secondsLeft === 0){
+      clearInterval(countDown);
+    }
+  },1000);
+}
 
-  function addPoints() {
-
-  }
-
-
-  function subtractTime() {
-
-  }
+btnStartEl.addEventListener("click", startQuiz)
