f61156456932ed84f9b5776983e913c1bf7c7cc0
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-04-22 16:15:47 -0500

    got the scoreboard working and finished

--- assets/script.js
+++ assets/script.js
@@ -1,100 +1,136 @@
-  var startButton = document.querySelector('.start-button');
-  var questionEl = document.getElementById('question');
-  var choiceButtons = document.getElementById('question-answers');
-  var timer = document.querySelector('.timer-count');
-  var secondsLeft = 15;
-  var score = 0;
-  var gameover;
-  var questionsAnswers = [];
-  var QuestionIndex = 0;
+var startButton = document.querySelector('.start-button');
+var questionContainer = document.getElementById('question');
+var quizButtons = document.getElementById('question-answers');
+var timer = document.querySelector('.timer-count');
+var questionsAnswers = [];
+var QuestionIndex = 0;
+var secondsLeft = 10;
 
-  var questionsAnswers = [
-    { q: 'What does "JS" stand for?', 
-      a: '1. JavaScript', 
-      choices: [{choice: '1. JavaScript '}, {choice: '2. '}, {choice: '3. '}, {choice: '4. '}]
-    },
-    { q: 'What does "CSS" stand for?', 
-      a: '3. Cascading Style Sheets', 
-      choices: [{choice: '1. '}, {choice: '2. '}, {choice: '3. Cascading Style Sheets'}, {choice: '4. '}]
-    },
-    { q: 'What does "HTML" stand for?', 
-      a: '2. Hypertext Markup Language', 
-      choices: [{choice: '1. '}, {choice: '2. Hypertext Markup Language'}, {choice: '3. '}, {choice: '4. '}]
-    },
-  ];
+var scoreboardEl = document.getElementById('scoreboard');
+var scores = JSON.parse(localStorage.getItem('scores')) || [];
+var initials;
+  
+//arr of questions, choices, and answers
+var questionsAnswers = [
+  { q: 'What does "JS" stand for?', 
+    a: '1. JavaScript', 
+    choices: [{choice: '1. JavaScript '}, {choice: '2. '}, {choice: '3. '}, {choice: '4. '}]
+  },
+  { q: 'What does "CSS" stand for?', 
+    a: '3. Cascading Style Sheets', 
+    choices: [{choice: '1. '}, {choice: '2. '}, {choice: '3. Cascading Style Sheets'}, {choice: '4. '}]
+  },
+  { q: 'What does "HTML" stand for?', 
+    a: '2. Hypertext Markup Language', 
+    choices: [{choice: '1. '}, {choice: '2. Hypertext Markup Language'}, {choice: '3. '}, {choice: '4. '}]
+  },
+];
 
 //deals with the countdown timer
 function startTimer() {
-  var countDown = setInterval(function(){
+  var countDown = setInterval(function() {
     secondsLeft--;
     timer.textContent = secondsLeft;
-    if(secondsLeft === 0){
+    if(secondsLeft === 0) {
       clearInterval(countDown);
-    }
+    };
   },1000);
-}
+};
 
 //hides intro page and shows the question page, the questions are then put in a random order. 
 function startQuiz() {
-    document.getElementById('intro-page').hidden = true;
-    document.getElementById('question-container').hidden = false;
-    questionsAnswers.sort(() => Math.random() - 0.5)
-    startTimer()
-    setQuestion()
-  }
-
-//displays the next question
-function setQuestion() {
-    resetAnswers()
-    displayQuestion(questionsAnswers[QuestionIndex])
-}
-
-//removes all the answer buttons
-function resetAnswers() {
-    while (choiceButtons.firstChild) {
-        choiceButtons.removeChild(choiceButtons.firstChild);
-    };
+  document.getElementById('intro-page').hidden = true;
+  document.getElementById('question-container').hidden = false;
+  questionsAnswers.sort(() => Math.random() - 0.5);
+  startTimer();
+  setQuestion();
 };
 
 //displays question information (including answer buttons)
 function displayQuestion(index) {
-    questionEl.innerText = index.q
-    for (var i = 0; i < index.choices.length; i++) {
-        var answerbutton = document.createElement('button')
-        answerbutton.innerText = index.choices[i].choice
-        answerbutton.classList.add('btn')
-        answerbutton.classList.add('answerbtn')
-        answerbutton.addEventListener('click', checkAnswr)
-        choiceButtons.appendChild(answerbutton)
-        }
+  questionContainer.innerText = index.q;
+  for (var i = 0; i < index.choices.length; i++) {
+    var answerbutton = document.createElement('button');
+      answerbutton.innerText = index.choices[i].choice;
+      answerbutton.classList.add('btn');
+      answerbutton.classList.add('answerbtn');
+      answerbutton.addEventListener('click', checkAnswr);
+      quizButtons.appendChild(answerbutton);
+  };
+};
+
+//displays the next question
+function setQuestion() {
+  resetAnswers();
+  displayQuestion(questionsAnswers[QuestionIndex]);
+};
+
+//checks if the answer is right or wrong and will then subtract time if its wrong  
+function checkAnswr(event) {
+  var selectedanswer = event.target;
+    if (questionsAnswers[QuestionIndex].a === selectedanswer.innerText){
+      console.log('yes');
+    }
+    else {
+      console.log('no');
+      secondsLeft = secondsLeft -2;
     };
 
-//check if answer is correct    
-function checkAnswr(event) {
-    var selectedanswer = event.target
-        if (questionsAnswers[QuestionIndex].a === selectedanswer.innerText){
-          console.log('yes');
-            score = score + 1
-        }
-        else {
-          console.log('no');
-          secondsLeft = secondsLeft -2;
+//goes to next question after a question is answered and checks if there are more questions
+    QuestionIndex++
+      if  (questionsAnswers.length > QuestionIndex + 0) {
+          setQuestion();
+      }   
+      else {
+        endGame();
       };
+};
 
-    //go to next question, check if there is more questions
-      QuestionIndex++
-        if  (questionsAnswers.length > QuestionIndex + 0) {
-            setQuestion()
-        }   
-        else {
-          endGame()
-            }
-}
+//removes all of the answer buttons
+function resetAnswers() {
+  while (quizButtons.firstChild) {
+      quizButtons.removeChild(quizButtons.firstChild);
+  };
+};
 
+//when game is over it hides the question page and the timer then makes the score page visible
 function endGame() {
+    document.getElementById('card-timer').hidden = true;
     document.getElementById('question-container').hidden = true;
     document.getElementById('scoreboard').hidden = false;
+    getUserInitials();
+};
+
+//
+function getUserInitials() {
+  initials = prompt("Game Over! Enter Your Initials");
+  addScoreToLocalStorage();
+};
+
+//
+function addScoreToLocalStorage() {
+  scores.push({
+    objInitials: initials,
+    objScore: secondsLeft
+  });
+  localStorage.setItem("scores", JSON.stringify(scores));
+  scoreboard();
+};
+
+//
+function scoreboard() {
+  let i = 0;
+  while (i < scores.length-1) {
+    i++;
+    var divEl = document.getElementById("scoreboard");
+    var pEl = document.createElement("p");
+    var leaderboard = JSON.parse(localStorage.getItem("scores"));
+    console.log(leaderboard);
+    pEl.textContent = leaderboard[i].objInitials + " " + leaderboard[i].objScore + "points";
+    divEl.append(pEl);
   }
+  //localStorage.clear();
+};
 
-startButton.addEventListener("click", startQuiz)
-
+//event listener for start button on intro page 
+startButton.addEventListener("click", startQuiz);
